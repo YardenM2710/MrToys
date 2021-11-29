541a204404870f2389d7b686c30f679bb89710cd
Yarden Mathieson <yardenm2710@gmail.com>
2021-11-29 19:07:45 +0200

    second commit

--- server.js
+++ server.js
@@ -16,22 +16,6 @@ const session = expressSession({
 app.use(express.json())
 app.use(session)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-  app.use(express.static(path.resolve(__dirname, 'public')))
-  const corsOptions = {
-    origin: [
-      'http://127.0.0.1:8080',
-      'http://localhost:8080',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-    ],
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
-}
-
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
 const reviewRoutes = require('./api/review/review.routes')
@@ -54,6 +38,22 @@ connectSockets(http, session)
 app.get('/**', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+  app.use(express.static(path.resolve(__dirname, 'public')))
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:8080',
+      'http://localhost:8080',
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
+}
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
