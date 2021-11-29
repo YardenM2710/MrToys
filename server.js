129d0e1418743af10402a0cfb5fa1cb6dd4bca42
Yarden Mathieson <yardenm2710@gmail.com>
2021-11-29 19:11:30 +0200

    second commit

--- server.js
+++ server.js
@@ -35,25 +35,25 @@ connectSockets(http, session)
 // Make every server-side-route to match the index.html
 // so when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
-} else {
-  app.use(express.static(path.resolve(__dirname, 'public')))
-  const corsOptions = {
-    origin: [
-      'http://127.0.0.1:8080',
-      'http://localhost:8080',
-      'http://127.0.0.1:3000',
-      'http://localhost:3000',
-    ],
-    credentials: true,
-  }
-  app.use(cors(corsOptions))
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.resolve(__dirname, 'public')))
+// } else {
+//   app.use(express.static(path.resolve(__dirname, 'public')))
+//   const corsOptions = {
+//     origin: [
+//       'http://127.0.0.1:8080',
+//       'http://localhost:8080',
+//       'http://127.0.0.1:3000',
+//       'http://localhost:3000',
+//     ],
+//     credentials: true,
+//   }
+//   app.use(cors(corsOptions))
+// }
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
