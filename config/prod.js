66d9bbef4b76d92823989fcff7b06fbdc32d33a5
Yarden Mathieson <yardenm2710@gmail.com>
2021-11-29 18:52:04 +0200

    second commit

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL:
+    'mongodb+srv://Yarden:@yardenm2710@cluster0.lsmqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
